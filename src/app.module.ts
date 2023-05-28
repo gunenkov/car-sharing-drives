9939e51c3de6466f360f056f7229bad9503b45e1
Mikhail Gunenkov <m.gunenkov@gmail.com>
2023-05-28 13:19:19 +0600

    Fix env

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { Drive } from './drives/models/drive.model';
         port: +configService.get<number>('PG_PORT'),
         username: configService.get('PG_USER'),
         password: configService.get('PG_PASSWORD'),
-        database: configService.get('PG_DB'),
+        database: configService.get('PG_DATABASE'),
         entities: [Drive],
         synchronize: true,
       }),
